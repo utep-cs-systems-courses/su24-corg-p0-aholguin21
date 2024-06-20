c8289290b111936bc87dc04feb8b74d6282a3778
aholguin21 <aholguin21@miners.utep.edu>
2024-06-20 10:58:38 -0600

    https://github.com/utep-cs-systems-courses/su24-corg-p1-tokenizer-aholguin21.git

--- draw_shapes.c
+++ draw_shapes.c
@@ -25,4 +25,8 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
+void print_arrow(int leftCol, int size){
+  print_triangle(leftCol, size);
+  print_square(leftCol+2, size+2);
+}
 
